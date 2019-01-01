189e841c4a1cc4f63972590ae1fcd5955543958d
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-01-01 04:12:12 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,5 @@
 # code the #greeting method here!
 def greeting(name)
-  put " Hi! I'm HAL, what's your name?' exactly."
+  puts " Hi! I'm HAL, what's your name?' exactly."
   puts " #{name}. It's nice to meet you."
 end
